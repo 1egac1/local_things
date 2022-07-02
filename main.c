c525860f19e0636647e4ac20d474d1ec9e583534
1egac1 <partyfoxmail@gmail.com>
2022-07-02 22:57:27 +0300

    Commit

--- main.c
+++ main.c
@@ -4,9 +4,7 @@
 #include <time.h>
 
 #define DIR_PATH "/home/q/Downloads"
-#define MODULE_PATH "/home/q/educ/cxx/local_things/modele.out"
-
-#define SIZE_OF_PATH_TO_EXE 200
+#define MODULE_PATH "/home/q/educ/cxx/local_things/model.out"
 
 void check_date(struct stat *date, time_t *l_chs);
 
@@ -18,6 +16,7 @@ int main (){
 	if (stat(DIR_PATH, &dates) == 0) 
 	{
 		check_date(&dates, &last_changes);
+		sleep(10);
 	}
 	else
 	{
@@ -32,8 +31,6 @@ void check_date(struct stat *date, time_t *l_chs)
 	if (date->st_ctime > *l_chs)
 	{
 		*l_chs = date->st_ctime;
-		char cdir[SIZE_OF_PATH_TO_EXE];
-		getcwd(cdir, SIZE_OF_PATH_TO_EXE);
 		execl(MODULE_PATH, MODULE_PATH, DIR_PATH, NULL);
 	}
 	else
--- module1.c
+++ module1.c
@@ -1,7 +1,39 @@
+#include <stddef.h>
 #include <stdio.h>
-#include <unistd.h>
+#include <dirent.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(){
-	
+int name_check(char *string);
+
+void register_normalize(char *string);
+
+int main(int argc, char *argv[])
+{
+	DIR *d;
+	struct dirent *dir;
+	d = opendir(argv[1]);
+
+	while((dir = readdir(d)) != NULL)
+	{
+		if(name_check(dir->d_name))
+			printf("%s\n", dir->d_name);
+	}
+	closedir(d);
+
+	return 0;
+}
+
+int name_check(char *string)
+{
+	size_t str_len = strlen(string);
+	char *string_end = string - 1 + str_len;
+
+	while (*string_end != '.' && string_end != string)
+	{
+		if(isupper(*string_end))
+			return 1;
+		--string_end;
+	}
 	return 0;
 }
